d1243a1e8bf3014f3f1ea508134fc272a9f88dcc
ronisdesousa <ronisdesousa@gmail.com>
2024-07-07 09:34:12 -0300

    [PrimeiroTeste] Concatenação

---
+++ TipoVariaveis/src/Operadores.java
@@ -0,0 +1,7 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String concatencao = "?";
+        concatencao = 1 + 1 + 1 + "1";
+        System.out.println(concatencao);
+    }
+}
