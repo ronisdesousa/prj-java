a11fdf96f81c5399892aa8be52c13e20981b764a
ronisdesousa <ronisdesousa@gmail.com>
2024-07-09 17:59:56 -0300

    DesafioBancario

--- TipoVariaveis/src/Operadores.java
+++
@@ -1,7 +0,0 @@
-public class Operadores {
-    public static void main(String[] args) {
-        String concatencao = "?";
-        concatencao = 1 + 1 + 1 + "1";
-        System.out.println(concatencao);
-    }
-}
